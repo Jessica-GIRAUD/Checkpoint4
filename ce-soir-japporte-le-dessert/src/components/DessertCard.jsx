d80837a42e51cf3067f378b46b3d0af74df9759f
Jessica GIRAUD <kikaaaa@macbook-pro.home>
2021-02-04 15:45:00 +0100

    creation component dessertCard + css

--- ce-soir-japporte-le-dessert/src/components/DessertCard.jsx
+++ ce-soir-japporte-le-dessert/src/components/DessertCard.jsx
@@ -1,7 +1,32 @@
-function DessertCard () {
+import React, { useEffect, useState } from "react";
+import "../styles/DessertCard.css";
+import axios from "axios";
+
+function DessertCard() {
+  const [desserts, setDesserts] = useState([]);
+
+  // Retrieve dessert from database
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/dessert/all`)
+      .then((res) => res.data)
+      .then((data) => setDesserts(data));
+  }, []);
+
   return (
-    <div>Hello</div>
-  )
+    <div className="page-container">
+      {desserts.map((dessert) => (
+        <div className="container">
+          <img src={dessert.photo} alt={dessert.name} className="dessert-image"/>
+          <h2 className="dessert-name">{dessert.name}</h2>
+          <div className="information-dessert">
+            <div className="difficulty">{dessert.difficulty_name}</div>
+            <div className="time-needed">{dessert.time_needed}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default DessertCard;
